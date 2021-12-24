e4a73e7856ba2e606e2a98f2cf894c86918f2b33
Nickita V <hihollow@yandex.ru>
2021-12-24 14:21:06 +0600

    Homeworklesson3
    Homeworklesson3

---
+++ Massive 1/Massive/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Massive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = { { 8, 2, 3 }, { 4, 5, 9 } };
+           int[] array1 = new int[6];
+            int n = -1;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+
+                    n ++;
+                    array1[n] = array[i, j];
+
+                }
+              
+                }
+
+            int sdvig = 0;
+            foreach (int f in array1)
+            {
+                Console.WriteLine(f.ToString().PadLeft(sdvig += 1));
+            }
+        }
+    }
+}
