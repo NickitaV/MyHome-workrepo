55abd1af4cd71858b5d1b6ad0d8159a0d1be9282
Nickita V <hihollow@yandex.ru>
2022-01-26 18:59:23 +0600

    Homework 6
    Homework 6

---
+++ Task M/Task M/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using System.Diagnostics;
+namespace Task_M
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+             
+            Task task = new Task();
+            task.Title();
+            Process[] proc = Process.GetProcesses();
+            task.Proc(proc);
+            task.FinalProc(proc);
+            }
+            catch (Exception) { Console.WriteLine("Error"); }
+        }
+    }
+}
---
+++ Task M/Task M/Task.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Diagnostics;
+
+    namespace Task_M
+{
+    public class Task 
+    {
+        public void Title() {
+            Console.WriteLine("название процесса (ID процесса)");
+            Console.WriteLine("===============================");
+            
+        }
+        public void Proc(Process[] proc) {
+            
+            
+                for (int i = 0; i < proc.Length; i++)
+                {
+                    Process process = proc[i];
+                    
+                    Console.WriteLine(process.ProcessName+"(ID: "+ process.Id+")");
+                   
+
+                }
+            
+        }
+        public void FinalProc(Process[] proc) {
+            Console.WriteLine("Введите название процесса, который вы хотели бы завершить или ID процесса:");
+            string p = Console.ReadLine();
+
+            for (int i = 0; i < proc.Length; i++)
+            {
+                if ((proc[i].ProcessName == p)||(proc[i].Id == Convert.ToInt32(p)))
+                {
+                    proc[i].Kill();
+                    Console.WriteLine("Работа процесса: " + proc[i] + " завершёна");
+                }
+
+            }
+        }
+    }
+}
