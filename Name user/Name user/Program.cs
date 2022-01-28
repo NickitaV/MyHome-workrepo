29e3d9f574ab0dfc2dcc0abb40af3bd410a52615
Nickita V <hihollow@yandex.ru>
2022-01-28 17:32:39 +0600

    Homework 8
    Homework 8

---
+++ Name user/Name user/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Name_user
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
